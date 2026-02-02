fe58d2e68c714f5d50a03069f0752f1678d7624a
24sbit099-sketch <24sbit099@psgrkcw.ac.in>
2026-02-02 15:38:24 +0530

    Implement caching for mean in cachematrix.R
    Added functions to cache mean and manage vector data.

--- cachematrix.R
+++ cachematrix.R
@@ -1,27 +1,41 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
-makeCacheMatrix <- function() {
- mat <- NULL
- inv_cache <- NULL
-
-setMatrix <- function(x) {
- mat <<- x
- inv_cache <<- NULL
+makeVector <- function(x = numeric()) {
+  m <- NULL
+  
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  
+  get <- function() {
+    x
+  }
+  
+  setmean <- function(mean) {
+    m <<- mean
+  }
+  
+  getmean <- function() {
+    m
+  }
+  
+  list(
+    set = set,
+    get = get,
+    setmean = setmean,
+    getmean = getmean
+  )
 }
-
-getInverse <- function() {
- if (is.null(inv_cache)) {
-  inv_cache <<- solve(mat)
- }
- inv_cache
-}
-
-list(setMatrix = setMatrix, getInverse = getInverse)
-}
-cacheSolve <- function(x, ...) {
- inv cache <- x$getInverse()
- inv_cache
+cachemean <- function(x, ...) {
+  m <- x$getmean()
+  
+  if (!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  
+  data <- x$get()
+  m <- mean(data, ...)
+  x$setmean(m)
+  
+  m
 }
